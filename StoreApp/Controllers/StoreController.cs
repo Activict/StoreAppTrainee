286fd4113f9aee2b0fad11d6708535d600efa49b
MaxPavlov <activict1@gmail.com>
2019-12-11 16:19:55 +0300

    FAT Create StoreController, List products in Index view

---
+++ StoreApp/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreApp.BLL.DTO;
+using StoreApp.BLL.Services;
+
+namespace StoreApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        private ProductService productService;
+        public StoreController()
+        {
+            productService = new ProductService();
+        }
+        // GET: Store
+        public ActionResult Index()
+        {
+            var products = productService.GetAll();
+
+            if (products == null)
+            {
+                ViewBag.Message = "No products";
+                return View();
+            }
+            else
+            {
+                return View(products);
+            }
+        }
+    }
+}
