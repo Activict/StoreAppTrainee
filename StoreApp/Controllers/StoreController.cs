06c5752a2c909c306bcaec004f5c936a3003cd08
MaxPavlov <activict1@gmail.com>
2019-12-11 17:11:23 +0300

    FAT Added CreateProduct view

--- StoreApp/Controllers/StoreController.cs
+++ StoreApp/Controllers/StoreController.cs
@@ -11,9 +11,17 @@ namespace StoreApp.Controllers
     public class StoreController : Controller
     {
         private ProductService productService;
+
+        private CategoryService categoryService;
+        private BrandService brandService;
+        private ProducerService producerService;
+
         public StoreController()
         {
             productService = new ProductService();
+            categoryService = new CategoryService();
+            brandService = new BrandService();
+            producerService = new ProducerService();
         }
         // GET: Store
         public ActionResult Index()
@@ -29,6 +37,36 @@ namespace StoreApp.Controllers
             {
                 return View(products);
             }
+        }
+
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(ProductDTO product)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "New product doesn't create");
+                return View(product);
+            }
+            if (product== null)
+            {
+                return View();
+            }
+
+            productService.Create(product);
+
+            ViewBag.Categories = new SelectList(categoryService.GetAll());
+            ViewBag.Brands = new SelectList(brandService.GetAll());
+            ViewBag.Producers = new SelectList(producerService.GetAll());
+
+            TempData["Message"] = "New product created seccessful!";
+
+            return View(product);
         }
     }
 }
