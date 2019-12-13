39035fdb18a220c16b38964afc00c6a08ffd9019
MaxPavlov <activict1@gmail.com>
2019-12-13 17:35:33 +0300

    FAT- Added CreateCategory view

--- StoreApp/Controllers/CategoryController.cs
+++ StoreApp/Controllers/CategoryController.cs
@@ -19,13 +19,39 @@ namespace StoreApp.Controllers
         {
             categoryService = new CategoryService();
 
-            config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoryDTO, CategoryViewModel>();}).CreateMapper();
+            config = new MapperConfiguration(cfg => { cfg.CreateMap<CategoryDTO, CategoryViewModel>();
+                                                      cfg.CreateMap<CategoryViewModel, CategoryDTO>();
+                                                    }).CreateMapper();
         }
         public ActionResult Index()
         {
             var categories = config.Map<IEnumerable<CategoryDTO>, IEnumerable<CategoryViewModel>>(categoryService.GetAll());
 
             return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCategory(CategoryViewModel category)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Category edit is wrong");
+                return View(category);
+            }
+
+            CategoryDTO categoryDTO = config.Map<CategoryViewModel, CategoryDTO>(category);
+
+            categoryService.Create(categoryDTO);
+
+            TempData["Message"] = "Category created success!";
+
+            return RedirectToAction("Index");
         }
     }
 }
