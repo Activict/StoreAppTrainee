9e3ab0e1b0ea2aaf8175763cd9a96a92fe83cd26
MaxPavlov <activict1@gmail.com>
2019-12-16 16:38:14 +0300

    FAT-5517 Created ListProducers View

---
+++ StoreApp/Controllers/ProducersController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using StoreApp.BLL.DTO;
+using StoreApp.BLL.Services;
+using StoreApp.Models.Producers;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace StoreApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private ProducerService producerService;
+        private IMapper config;
+
+        public ProducersController()
+        {
+            producerService = new ProducerService();
+
+            config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ProducerDTO, ProducerViewModel>();
+                cfg.CreateMap<ProducerViewModel, ProducerDTO>();
+            }).CreateMapper();
+        }
+
+        public ActionResult Index()
+        {
+            var producers = config.Map<IEnumerable<ProducerDTO>, IEnumerable<ProducerViewModel>>(producerService.GetAll());
+
+            return View(producers);
+        }
+    }
+}
---
+++ StoreApp/Models/Producers/ProducerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreApp.Models.Producers
+{
+    public class ProducerViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
