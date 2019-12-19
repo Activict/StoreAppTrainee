c81022aa9b530a045aafea6225bcfe4ba9d81c6b
MaxPavlov <activict1@gmail.com>
2019-12-19 16:30:17 +0300

    FAT-5569 Created Dropdawn Units in Product views

--- StoreApp.BLL/DTO/ProductDTO.cs
+++ StoreApp.BLL/DTO/ProductDTO.cs
@@ -6,11 +6,11 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public string Unit { get; set; }
         public string Picture { get; set; }
         public string Quality { get; set; }
         public bool Enable { get; set; }
 
+        public int UnitId { get; set; }
         public int CategoryId { get; set; }
         public int BrandId { get; set; }
         public int ProducerId { get; set; }
